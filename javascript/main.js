14a0229de3bf10ae43ba7a88f09aa718a1e777a0
sulaiman <mdsulaiman132k@gmail.com>
2021-02-27 11:31:46 +0530

    upgrade in js

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,29 @@
+var  img = "C:/Users/mohamedsulaiman/Documents/webdevelopment/foodService/images/"
+ var fooditems = {
+  pizza : {
+    vegpizza : {
+      name : "Veg.Pizza"  ,
+      image : "pizza3.jpg" ,
+      rupee : 200 ,
+      description : "Foods Hot Spice And Delicious Pizza"
+    },
+     pizza : {
+       name : "Delicious Veg.Pizza" ,
+       image : "pizza2.jpg" ,
+       rupee : 250 ,
+       description : "Spicy Pizza Hut to Add 25 Percent More Cheese "
+     },
+     chickenpizza : {
+      name : "Crispy Chicken.Pizza"  ,
+      image : "pizza.jpg" ,
+      rupee :  300,
+      description : "Chicken Tikka, Chicken Malai Tikka, Onion, Red Paprika"
+    }
+
+  }
+
+ };
+
  function search() {
  	     clr = document.querySelector("#search").style.color 
  	     if(clr != "rgb(255, 167, 0)"){
@@ -15,10 +41,38 @@
             $("#visi").addClass("visible") ;
             $("#hide").removeClass("vis") ;
              $("#hide").addClass("hide")  ;
-
-
    	  	 }
 
-
-
    	  }
+
+  $("document").ready( function(){
+      $("#sear").on("keyup" , function(event) {
+         if(event.key === 'Enter'){
+               for(var i in fooditems){
+                   if(i == $("#sear").val()){
+                      var cnt = 1 ;
+                      var obj = fooditems[i]
+                      for(var j in obj){
+                         var obj2 = obj[j]
+                         for(var k in obj2){
+                             if(k == "name") {
+                               $("#foodhead"+cnt).html(obj2[k]) ;}
+                             else if(k == "image"){
+                                $("#food"+cnt).attr("src" , img + obj2[k]) ;
+                             }
+                             else if(k == "rupee")
+                                 $("#foodprice"+cnt).html("Rs:" + obj2[k]) ;
+                              else if(k == "description")
+                                  $("#foodbody"+cnt).html(obj2[k]) ;
+                         }
+                         cnt++ ;
+
+                      }
+                      break ;
+                   }
+
+               }
+         }
+      });
+
+  });
